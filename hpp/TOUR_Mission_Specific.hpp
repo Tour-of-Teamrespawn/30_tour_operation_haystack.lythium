a86f8be07d4480bbce0d663728818b55abf557ec
Andy455 <andy.dlp93@gmail.com>
2022-08-12 22:18:44 +0100

    Updated build script to latest. v0.7

--- hpp/TOUR_Mission_Specific.hpp
+++ hpp/TOUR_Mission_Specific.hpp
@@ -8,10 +8,10 @@ class Header
 
 // Lobby Settings
 author = "Andy455 [Tour]";
-onLoadName = "30 [Tour] Operation Haystack v0.3";
+onLoadName = "30 [Tour] Operation Haystack v0.7";
 onLoadMission = "SOCOM has an urgent priority mission to capture an insurgent leader."; 
 loadScreen = "img\loadingScreen.paa";
-briefingName = "30 [Tour] Operation Haystack v0.3";
+briefingName = "30 [Tour] Operation Haystack v0.7";
 enableDebugConsole[] = {"76561198000647052"};
 
 // Mission Select Info
